7897cb9678032a1cdd76fde67b43ef6c57573c31
Landry Geiger <landry.geiger@gmail.com>
2024-02-17 13:40:12 -0600

    Show stream on UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,23 @@
+import { useEffect, useRef } from "react";
+
 const App = () => {
-  return <h1>Hello World!</h1>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+      if (!videoRef.current) return;
+      videoRef.current.srcObject = stream;
+
+      const mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.ondataavailable = e => {
+        if (e.data.size > 0) console.log("sending");
+      };
+
+      mediaRecorder.start(100);
+    });
+  }, []);
+
+  return <video ref={videoRef} style={{ width: "100%" }} autoPlay />;
 };
 
 export default App;
