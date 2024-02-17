7770a94d30156b3c9945359a28c6152785662f0d
john <johnmichaelpost@gmail.com>
2024-02-17 14:04:45 -0600

    connect client ws demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,18 @@ const App = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
+    const ws = new WebSocket("ws://localhost:4444");
+    ws.onmessage = console.log;
+
     navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
       if (!videoRef.current) return;
       videoRef.current.srcObject = stream;
 
       const mediaRecorder = new MediaRecorder(stream);
       mediaRecorder.ondataavailable = e => {
-        if (e.data.size > 0) console.log("sending");
+        if (e.data.size > 0) {
+          e.data.arrayBuffer().then(x => ws.send(x));
+        }
       };
 
       mediaRecorder.start(100);
