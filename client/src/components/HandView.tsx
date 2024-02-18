c1d394b66883e20c62b14bde9689ebec1ab255cc
Landry Geiger <landry.geiger@gmail.com>
2024-02-17 21:56:39 -0600

    Create chip

--- client/src/components/HandView.tsx
+++ client/src/components/HandView.tsx
@@ -1,5 +1,7 @@
 import { CSSProperties, FC } from "react";
 import { Hand } from "../types";
+import { Chip } from "@mui/joy";
+import { LightbulbOutlined } from "@mui/icons-material";
 
 const CARD_WIDTH = "7vw";
 const CARD_HEIGHT = "9.96vw";
@@ -21,35 +23,56 @@ const HandView: FC<Props> = ({ hands, style }) => {
     <div
       style={{
         display: "flex",
-        ...style,
+        flexDirection: "column",
         alignItems: "center",
-        justifyContent: "space-around",
+        ...style,
       }}
     >
-      {hands.map(hand => (
-        <div
-          style={{
-            width: handWidthCssCalcStr(hand),
-            height: handHeightCssCalcStr(hand),
-            position: "relative",
-          }}
-        >
-          {hand.map((card, i) => (
-            <img
-              src={`/${card.suit}_${card.rank}.svg`}
-              style={{
-                display: "block",
-                width: CARD_WIDTH,
-                height: CARD_HEIGHT,
-                position: "absolute",
-                top: `calc(${i} * ${CARD_SPACING}`,
-                left: `calc(${i} * ${CARD_SPACING}`,
-                boxShadow: "0.2em 0.2em 0.2em rgba(0, 0, 0, .2)",
-              }}
-            />
-          ))}
-        </div>
-      ))}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          alignSelf: "stretch",
+          justifyContent: "space-around",
+          flexGrow: 1,
+        }}
+      >
+        {hands.map(hand => (
+          <div
+            style={{
+              width: handWidthCssCalcStr(hand),
+              height: handHeightCssCalcStr(hand),
+              position: "relative",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {hand.map((card, i) => (
+              <img
+                src={`/${card.suit}_${card.rank}.svg`}
+                style={{
+                  display: "block",
+                  width: CARD_WIDTH,
+                  height: CARD_HEIGHT,
+                  position: "absolute",
+                  top: `calc(${i} * ${CARD_SPACING}`,
+                  left: `calc(${i} * ${CARD_SPACING}`,
+                  boxShadow: "0.2em 0.2em 0.2em rgba(0, 0, 0, .2)",
+                }}
+              />
+            ))}
+            <Chip
+              color="primary"
+              startDecorator={<LightbulbOutlined fontSize="small" />}
+              variant="soft"
+              sx={{ zIndex: 3 }}
+            >
+              Stand
+            </Chip>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
